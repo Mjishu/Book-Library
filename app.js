cda9d35264a745795d9c4245c631679d95ef28cf
Mjishu <128941980+Mjishu@users.noreply.github.com>
2023-10-30 22:32:10 -0400

    outline underway

---
+++ app.js
@@ -0,0 +1,10 @@
+
+//book Constructor
+function Book(title, author, pages, status){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+}
+
+let myLibrary = []
